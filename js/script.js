253f28c83e1740a0d07d96f08be8572073441cba
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-04-08 18:17:53 +0200

    Bonus 2 completo
    Cercando su MDN docs ho trovato un impiego del metodo Sort per ordinare oggetti all'interno di array, confrontando tramite una funzione di callback i valori assunti rispettivamente tra le loro proprietà. Riordinato l'array estrapolato dall'API, l ho poi assegnato come valore di Albums, andando a modificare la situazione dell'html.

--- js/script.js
+++ js/script.js
@@ -17,13 +17,22 @@ const App = new Vue ({
   mounted: function(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((dischi) => {
-        this.albums = dischi.data.response;
+        //assegno i dati ottenuto dall' API tramitre richiesta AJAX a una const, per semplificare i passaggi successivi.
+        const Dati = dischi.data.response;
+        //questa parte di funzione usa Sort per ordinare gli oggetti di un array secondo il valore di una delle loro proprietà - MDN WEBDOCS
+        Dati.sort(function (a, b) {
+          return a.year - b.year;
+        });
+        // attribuisco i dati estrapolati dall'API e ordinati secondo l'anno alla variabie albums data che mi permetterà di stamparli
+          this.albums = Dati;
         // questa parte di funzione serve per prelevare i generi musicali e salvarli nell array genres
-        dischi.data.response.forEach((item, i) => {
+        Dati.forEach((item, i) => {
           if (!this.genres.includes(item.genre)) {
             this.genres.push(item.genre);
           }
         });
+        // lascio qui questi console log che servono per controllo
+        console.log(this.albums);
         console.log(this.genres);
       });
   },
